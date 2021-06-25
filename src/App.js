eac3acdc533e60236476ff62e88f5eebef675f8f
galihproo <galihprihananto29@gmail.com>
2021-06-25 20:15:49 +0700

    clean up style app.js

--- src/App.js
+++ src/App.js
@@ -2,34 +2,16 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 
+import './App.css';
+
 function App() {
   return (
-    <>
-      <style>
-        {`
-          @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
-
-          body {
-            margin: 0;
-            padding: 0;
-            font-family: 'Inter', sans-serif;
-          }
-
-          .btn-primary {
-            background-color: #0071BC;
-            color: #fff;
-            font-weight: bold;
-            border-radius: 12px;
-          }
-        `}
-      </style>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/kontak" exact component={Contact} />
-        </Switch>
-      </Router>
-    </>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/kontak" exact component={Contact} />
+      </Switch>
+    </Router>
   );
 }
 
